d7bffa76c8cc8a04ee665602a53fe7ce2bc16059
crysb <crysbae@gmail.com>
2021-04-12 15:37:40 -0700

    updated SBSOD scoring

--- SBSOD_scoring.R
+++ SBSOD_scoring.R
@@ -3,15 +3,14 @@
 ##############################################
 #
 # Scoring script by Crystal Bae
-# Last updated October 2019
+# Last updated April 2021
 #
-# Citation for SBSOD: 
+# Citation for the SBSOD: 
 # Hegarty, M., Richardson, A. E., Montello, D. R., Lovelace, K., & Subbiah, I. (2002). Development of a self-report measure of environmental spatial ability. Intelligence, 30(5), 425–447. https://doi.org/10.1016/S0160-2896(02)00116-2
 #
-# Questionnaire itself is also available in the Hegarty et al. (2002) paper, on pages 445-446.
+# Questionnaire itself is available in the Hegarty et al. (2002) paper, on pages 445-446.
 #
-# Notes:
-# ------
+# Scoring:
 # Items are scored on a Likert scale ranging from 1 to 7:
 #   1 = strongly agree
 #   ...
@@ -22,87 +21,67 @@
 
 
 #########
-# Step 1. Load the tidyverse
-# Note: install.packages("tidyverse") if first time using tidyverse on your machine
+# Step 1. Load packages
+
+#install.packages("tidyverse") # e.g. if first time using tidyverse on your machine
 
 library(tidyverse)
+library(dplyr)
+library(here)
+library(data.table)
 
 
 #########
 # Step 2. Read in data
 
-# If your data is in another directory:
-# setwd("R:/users/cbae/DATA STORAGE/BigFiveInventory")
+# If your data is in another directory, use `here` package:
+#df <- read_csv(here("foldername", "filename.csv"))
 
-df <- read_csv("Pre-Study-Questionnaire.csv") ## Change filename to whatever you named it
+df <- read_csv("Pre-Study-Questionnaire.csv") # change filename to whatever you named it
+
+#########
+# Step 3. Subset dataframe and check data
+
+# Subset SBSOD questions and give them names 'sbsod_1' through 'sbsod_15'
+setnames(df, old = c(10:24),  ## Change this based on which 15 columns of your dataframe contain SBSOD questions
+         new = c('sbsod_1', 'sbsod_2', 'sbsod_3', 'sbsod_4', 'sbsod_5', 'sbsod_6', 'sbsod_7', 'sbsod_8', 'sbsod_9', 'sbsod_10', 'sbsod_11', 'sbsod_12', 'sbsod_13', 'sbsod_14', 'sbsod_15'))
+
+# Check data for missing or impossible values
+summary(df[,10:24]) # Min. should not be < 1 and Max. should not be > 7 for any question
+# data_all %>% 
+#   select(., contains('sbsod_')) %>% 
+#   summary()
 
 
 #########
-# Step 3. Subset dataframe
-
-# Subset basic demographics
-PID <- df[,3]
-date <- df[,4]
-sex <- df[,8]
-age <- df[,9]
-
-# Subset SBSOD questions:
-SODall <- df[,10:24] ## Change this based on which columns of your dataframe contain SBSOD questions, 1 - 15
-
-# Check data for missing / impossible values
-summary(SODall) # Min. should not be < 1 and Max. should not be > 7 for any question
-
-# Assign each SBSOD question
-
-SOD01 <- SODall[,1]
-SOD02 <- SODall[,2]
-SOD03 <- SODall[,3]
-SOD04 <- SODall[,4]
-SOD05 <- SODall[,5]
-SOD06 <- SODall[,6]
-SOD07 <- SODall[,7]
-SOD08 <- SODall[,8]
-SOD09 <- SODall[,9]
-SOD10 <- SODall[,10]
-SOD11 <- SODall[,11]
-SOD12 <- SODall[,12]
-SOD13 <- SODall[,13]
-SOD14 <- SODall[,14]
-SOD15 <- SODall[,15]
-
-
-#########
-# Step 4. Reverse-coded questions
+# Step 4. Reverse-code questions
 # Questions 1, 3, 4, 5, 7, 9, 14 need to be reverse-scored
 
 # Reverse coding:
-SOD01 <- 8-SOD01
-SOD03 <- 8-SOD03
-SOD04 <- 8-SOD04
-SOD05 <- 8-SOD05
-SOD07 <- 8-SOD07
-SOD09 <- 8-SOD09
-SOD14 <- 8-SOD14
+df$sbsod_1 <- 8-df$sbsod_1
+df$sbsod_3 <- 8-df$sbsod_3
+df$sbsod_4 <- 8-df$sbsod_4
+df$sbsod_5 <- 8-df$sbsod_5
+df$sbsod_7 <- 8-df$sbsod_7
+df$sbsod_9 <- 8-df$sbsod_9
+df$sbsod_14 <- 8-df$sbsod_14
 
 
 #########
 # Step 5. Scored SBSOD for each participant
 # The final SBSOD score for each participant will be between 1 and 7
-# NOTE: Give this a proper header row!
 
-SBSOD <- (SOD01+SOD02+SOD03+SOD04+SOD05+SOD06+SOD07+SOD08+SOD09+
-            SOD10+SOD11+SOD12+SOD13+SOD14+SOD15)/15
+# Sum all questions and divide by # of questions (15)
+# Adds SBSOD scores to existing dataframe
+df <- df %>%
+  mutate(sbsod = ((rowSums(select(., contains('sbsod_'))))/15))
 
-summary(SBSOD)
+summary(df$sbsod)
 
 
 #########
 # Step 6. Output to CSV
-# Outputs a CSV table with PID and SBSOD
+# Outputs new CSV with calculated SBSOD scores
 
-output <- data.frame(PID[], SBSOD[])
-colnames(output) <- c("PID", "SBSOD")
-
-# path = filename to be written
-write_csv(output, path = "output_SBSOD.csv", append = FALSE, col_names = TRUE)
+write_csv(df, file = "output.csv", append = FALSE, col_names = TRUE)
 
